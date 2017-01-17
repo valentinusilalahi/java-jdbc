be06f020c3e10bfc31cbbc7a33afc3de2327f172
valentinus silalahi <valentinus.silalahi@gmail.com>
2017-01-17 17:47:44 +0700

    domain object

--- java-database/src/main/java/com/silalahi/valentinus/database/domain/Penjualan.java
+++ java-database/src/main/java/com/silalahi/valentinus/database/domain/Penjualan.java
@@ -1,5 +1,36 @@
 package com.silalahi.valentinus.database.domain;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Penjualan {
-	
+	private Integer id;
+	private Date waktuTransaksi;
+	private List<PenjualanDetail> listPenjualan = new ArrayList<PenjualanDetail>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getWaktuTransaksi() {
+		return waktuTransaksi;
+	}
+
+	public void setWaktuTransaksi(Date waktuTransaksi) {
+		this.waktuTransaksi = waktuTransaksi;
+	}
+
+	public List<PenjualanDetail> getListPenjualan() {
+		return listPenjualan;
+	}
+
+	public void setListPenjualan(List<PenjualanDetail> listPenjualan) {
+		this.listPenjualan = listPenjualan;
+	}
+
 }
